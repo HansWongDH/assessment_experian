991618ec43cbb3449290c7a11ca217c9fb43c34c
Hans Wong <79557131+HansWongDH@users.noreply.github.com>
2023-11-27 06:28:52 +0800

    Feat/backend/unit test (#3)
    * added unit test for UserController
    
    * fixed invalid database bug

--- backend/src/main/java/com/example/backend/user/dto/UserDto.java
+++ backend/src/main/java/com/example/backend/user/dto/UserDto.java
@@ -5,4 +5,13 @@ public class UserDto {
 	public String firstName;
 	public String lastName;
 	public String email;
+
+	public UserDto() {};
+	
+	public UserDto(Long id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
 }
